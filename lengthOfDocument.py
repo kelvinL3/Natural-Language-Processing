78d26fc2064a91c4692f8cc56bf8a3b7f22c10cc
Kelvin <kelvinliu1234@gmail.com>
2019-04-14 16:56:15 -0400

    Refactored length_of_document

--- lengthOfDocument.py
+++ lengthOfDocument.py
@@ -1,3 +1,7 @@
+import re
 
-def lengthOfDocument(lyricAsString):
-	return len(lyricAsString.split())
+def length_of_document(lyric_as_string):
+	return len(re.findall(r"[\w']+", lyric_as_string))
+	
+# if __name__ == "__main__":
+# 	print(length_of_document(""))
