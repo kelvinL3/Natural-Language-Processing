c68f7de6dd49128b1dbb79ca08ca624fb4096d9b
Kelvin <kelvinliu1234@gmail.com>
2019-04-15 08:55:26 -0400

    comment https://github.com/kelvinL3/Genre-Classifier/pull/5#pullrequestreview-226428236 fix, CR LF fix

--- feature_NER.py
+++ feature_NER.py
@@ -32,19 +32,29 @@ entity_to_index_map = {
 
 
 def named_entity_recognition(lyric_string):
+	
+	missing = {}
+	
 	doc = nlp(lyric_string)
 	# print([(X.text, X.label_) for X in doc.ents])
-	print([X.label_ for X in doc.ents])
+	# print([X.label_ for X in doc.ents])
 	vector = [0] * len(entity_to_index_map)
 	for X in doc.ents:
 		if X.label_ not in entity_to_index_map:
-			print("Error, entity {} does not appear in entity_to_index_map".format(X.label_))
-			exit()
-		vector[entity_to_index_map[X.label_]]+=1
-	print(vector)
+			if X.label_ not in missing:
+				print("Error, entity {} does not appear in entity_to_index_map".format(X.label_))
+				missing[X.label_] = 1
+			else:
+				missing[X.label_] += 1
+		else:
+			vector[entity_to_index_map[X.label_]]+=1
+	
+	if missing:
+		print("There are unrecognized labels from spacy {}".format(missing))
+	# print(vector)
 	return vector
 	
 
 
-# if __name__ == "__main__":
-# 	named_entity_recognition("Angela Merkel reached a deal with EU negotiators to end the trade imbalance.")
+if __name__ == "__main__":
+	print(named_entity_recognition("Angela Merkel reached a deal with EU negotiators to end the trade imbalance."))
