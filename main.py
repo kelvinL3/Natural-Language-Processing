c79c352374a3a6c964debf94f917e58f788e9043
Aditya Shastri <ad.shastri1@gmail.com>
2019-04-17 14:25:17 -0400

    structure

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from feature_NER import named_entity_recognition
 from word_embeddings import get_word_embeddings
 from lengthOfDocument import length_of_document
+import csv
 
+def store_features(lyrics):
+    # do stuff
+    return 0
+
+with open("lyrics.csv") as lyrics:
+    reader = csv.reader(lyrics)
+    next(reader, None)
+    for line in reader:
+        store_features(line[-1])
